8aa3a7b0f69d1ca4d021aae4111745fd0cf323b9
Irkham Widhi <fishblacklance@gmail.com>
2019-03-16 16:43:24 +0700

    part2 - register to db

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,8 +1,8 @@
-import { Entity, Column, PrimaryColumn, BeforeInsert } from "typeorm";
+import { Entity, Column, PrimaryColumn, BeforeInsert, BaseEntity } from "typeorm";
 import * as uuidv4 from "uuid/v4";
 
-@Entity()
-export class User {
+@Entity("users")
+export class User extends BaseEntity {
 
     @PrimaryColumn("uuid")
     id: string;
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,21 @@
+import * as bcrypt from 'bcryptjs'
 import { ResolverMap } from './types/schema-utils';
+import { User } from './entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || 'World'}`,
     },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {}
+    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        email: email,
+        password: hashedPassword
+      })
+
+      await user.save()
+      return true
     }
+  }
 }
